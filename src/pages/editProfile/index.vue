268efc1b8249c7f55a1b58eb108f964e07b6d44f
fly3949 <i@fly.moe>
2021-03-02 13:41:01 +0800

    fix: upload avatar

--- src/pages/editProfile/index.vue
+++ src/pages/editProfile/index.vue
@@ -184,6 +184,10 @@ const uploadAvatar = async (path: string) => {
   }
 };
 
+const handleAvatarChange = (path: any) => {
+  uploadAvatar(path);
+};
+
 export default defineComponent({
   components: { UCellGroup, UCellItem },
   setup() {
@@ -200,9 +204,10 @@ export default defineComponent({
     };
   },
   onLoad() {
-    bus.on("uAvatarCropper", (path) => {
-      uploadAvatar(path);
-    });
+    bus.on("uAvatarCropper", handleAvatarChange);
+  },
+  onUnload() {
+    bus.off("uAvatarCropper", handleAvatarChange);
   },
 });
 </script>
